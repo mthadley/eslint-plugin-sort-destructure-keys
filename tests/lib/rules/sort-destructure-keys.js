357e7512294a7c0d36381ad8d621a1785b1410b3
Michael Hadley <mikethadley@gmail.com>
2019-07-02 19:20:58 -0700

    Organize test suite around specific cases

--- tests/lib/rules/sort-destructure-keys.js
+++ tests/lib/rules/sort-destructure-keys.js
@@ -12,231 +12,287 @@ function just(...args) {
   return [msg(...args)];
 }
 
-const ruleTester = new RuleTester({
-  parserOptions: {
-    ecmaVersion: 2018
-  }
-});
-
-ruleTester.run("sort-destructure-keys", rule, {
-  valid: [
-    `
-      const {
-          a,
-          b
-      } = someObj;
-    `,
-    "const {a, b} = someObj;",
-    "const {a, b: c} = someObj;",
-    "const {aBc, abd} = someObj;",
-    "const {1: a, 2: b} = someObj;",
-    "const {a: foo, b} = someObj;",
-    "const {b, a = b} = someObj;",
-    "const {a = {}, b = {}} = someObj;",
-    "const {a, ...other} = someObj;",
-    "const {a, b, ...other} = someObj;",
-    "const {...other} = someObj;",
-    "const func = ({a, b}) => a + b;",
-    "const {a: {b, c}, d: {e, f: {g}}} = someObj;",
-    "const {a, ['b']: x} = someObj;",
-    "const {['a']: {c: d}, b} = someObj;",
-    "const {a, [`b${foo}`]: x} = someObj;",
-
-    // Can't sort these because they depend on previous properties
-    "const {b, ['a']: {x = b}} = someObj;",
-    "const {a, c: {e, d = e}, b} = someObj;",
-    `
-      const {
-        a,
-        b,
-        ['c']: x,
-        ['d']: y,
-        [\`\${e}foo\`]: z
-      } = someObj;
-    `,
-    `
-      const {
-        a,
-        [\`\${e}foo\`]: y,
-        [\`\${d}foo\`]: z
-      } = someObj;
-    `,
-    `
-      const {
-        a,
-        [\`\${d}foo\`]: z,
-        [\`\${e}foo\`]: y
-      } = someObj;
-    `,
-    {
-      code: "const {a, b} = someObj;",
-      options: [{ caseSensitive: true }]
-    },
-    {
-      code: "const {B, a} = someObj;",
-      options: [{ caseSensitive: true }]
-    },
-    {
-      code: "const {aCc, abb} = someObj;",
-      options: [{ caseSensitive: true }]
+describe("sort-destructure-keys", () => {
+  const ruleTester = new RuleTester({
+    parserOptions: {
+      ecmaVersion: 2018
     }
-  ],
-  invalid: [
-    {
-      code: `
-        const {
-            b,
-            a
-        } = someObj;
-      `,
-      errors: just("b", "a"),
-      output: `
+  });
+
+  const testsFor = (name, ...args) => ruleTester.run(name, rule, ...args);
+
+  testsFor("basic sorting", {
+    valid: [
+      `
         const {
             a,
             b
         } = someObj;
-      `
-    },
-    {
-      code: `
-        const {
-            b,
-            a = 3,
-            c
-        } = someObj;
       `,
-      errors: just("b", "a"),
-      output: `
-        const {
-            a = 3,
-            b,
-            c
-        } = someObj;
+      "const {a, b} = someObj;",
+      "const {a, b: c} = someObj;",
+      "const {aBc, abd} = someObj;",
+      "const {a: foo, b} = someObj;",
+      "const func = ({a, b}) => a + b;"
+    ],
+    invalid: [
+      {
+        code: "const {b, a} = someObj;",
+        errors: just("b", "a"),
+        output: "const {a, b} = someObj;"
+      },
+      {
+        code: "const {a, c, b} = someObj;",
+        errors: just("c", "b"),
+        output: "const {a, b, c} = someObj;"
+      },
+      {
+        code: `
+          const {
+              b,
+              a = 3,
+              c
+          } = someObj;
+        `,
+        errors: just("b", "a"),
+        output: `
+          const {
+              a = 3,
+              b,
+              c
+          } = someObj;
+        `
+      },
+      {
+        code: "const func = ({b, a}) => a + b;",
+        errors: just("b", "a"),
+        output: "const func = ({a, b}) => a + b;"
+      }
+    ]
+  });
+
+  testsFor("rest properties", {
+    valid: [
+      "const {a, ...other} = someObj;",
+      "const {a, b, ...other} = someObj;",
+      "const {...other} = someObj;"
+    ],
+    invalid: [
+      {
+        code: "const {b, a, ...rest} = someObj;",
+        errors: just("b", "a"),
+        output: "const {a, b, ...rest} = someObj;"
+      },
+      {
+        code: `
+          function foo({
+              b = false,
+              a,
+              ...rest
+          }) {}
+        `,
+        errors: just("b", "a"),
+        output: `
+          function foo({
+              a,
+              b = false,
+              ...rest
+          }) {}
+        `
+      }
+    ]
+  });
+
+  testsFor("key literals", {
+    valid: [
+      "const {1: a, 2: b} = someObj;",
+      "const {'a': a, 'b': b} = someObj;"
+    ],
+    invalid: [
+      {
+        code: "const {2: b, 1: a} = someObj;",
+        errors: just("2", "1"),
+        output: "const {1: a, 2: b} = someObj;"
+      },
+      {
+        code: "const {'b': b, 'a': a} = someObj;",
+        errors: just("b", "a"),
+        output: "const {'a': a, 'b': b} = someObj;"
+      }
+    ]
+  });
+
+  testsFor("default literals", {
+    valid: ["const {b, a = b} = someObj;", "const {a = {}, b = {}} = someObj;"],
+    invalid: [
+      {
+        code: "const {a, c, b = 3} = someObj;",
+        errors: just("c", "b"),
+        output: "const {a, b = 3, c} = someObj;"
+      }
+    ]
+  });
+
+  testsFor("identifier literals", {
+    valid: [
+      "const {b, ['a']: {x = b}} = someObj;",
+      "const {a, c: {e, d = e}, b} = someObj;"
+    ],
+    invalid: [
+      {
+        code: "const {a, c: {e, d}, b = c} = someObj;",
+        errors: [msg("e", "d")],
+        output: "const {a, c: {d, e}, b = c} = someObj;"
+      }
+    ]
+  });
+
+  testsFor("nested object patterns", {
+    valid: ["const {a: {b, c}, d: {e, f: {g}}} = someObj;"],
+    invalid: [
+      {
+        code: "const {a, b, c: {e, d}} = someObj;",
+        errors: just("e", "d"),
+        output: "const {a, b, c: {d, e}} = someObj;"
+      },
+      {
+        code: "const {a, c: {e, d}, b} = someObj;",
+        errors: [msg("e", "d"), msg("c", "b")],
+        output: "const {a, b, c: {e, d}} = someObj;"
+      },
+      {
+        code: `
+          const {
+              a,
+              b: {
+                  e,
+                  d
+              },
+              c
+          } = someObj;
+        `,
+        errors: just("e", "d"),
+        output: `
+          const {
+              a,
+              b: {
+                  d,
+                  e
+              },
+              c
+          } = someObj;
+        `
+      }
+    ]
+  });
+
+  testsFor("computed property literals", {
+    valid: [
+      "const {a, ['b']: x} = someObj;",
+      "const {['a']: {c: d}, b} = someObj;"
+    ],
+    invalid: [
+      {
+        code: "const {b, ['a']: x, z} = someObj;",
+        errors: just("b", "a"),
+        output: "const {['a']: x, b, z} = someObj;"
+      }
+    ]
+  });
+
+  testsFor("computed properties", {
+    valid: [
+      "const {a, [`b${foo}`]: x} = someObj;",
       `
-    },
-    {
-      code: `
-        const {
-            a,
-            b: {
-                e,
-                d
-            },
-            c
-        } = someObj;
-      `,
-      errors: just("e", "d"),
-      output: `
-        const {
-            a,
-            b: {
-                d,
-                e
-            },
-            c
-        } = someObj;
-      `
-    },
-    {
-      code: "const {b, a} = someObj;",
-      errors: just("b", "a"),
-      output: "const {a, b} = someObj;"
-    },
-    {
-      code: "const func = ({b, a}) => a + b;",
-      errors: just("b", "a"),
-      output: "const func = ({a, b}) => a + b;"
-    },
-    {
-      code: "const {a, c, b} = someObj;",
-      errors: just("c", "b"),
-      output: "const {a, b, c} = someObj;"
-    },
-    {
-      code: "const {a, c, b = 3} = someObj;",
-      errors: just("c", "b"),
-      output: "const {a, b = 3, c} = someObj;"
-    },
-    {
-      code: "const {a, b, c: {e, d}} = someObj;",
-      errors: just("e", "d"),
-      output: "const {a, b, c: {d, e}} = someObj;"
-    },
-    {
-      code: "const {a, c: {e, d}, b} = someObj;",
-      errors: [msg("e", "d"), msg("c", "b")],
-      output: "const {a, b, c: {e, d}} = someObj;"
-    },
-    {
-      code: "const {a, c: {e, d}, b = c} = someObj;",
-      errors: [msg("e", "d")],
-      output: "const {a, c: {d, e}, b = c} = someObj;"
-    },
-    {
-      code: "const {b, a} = someObj;",
-      errors: just("b", "a"),
-      output: "const {a, b} = someObj;",
-      options: [{ caseSensitive: true }]
-    },
-    {
-      code: "const {a, B} = someObj;",
-      errors: just("a", "B"),
-      output: "const {B, a} = someObj;",
-      options: [{ caseSensitive: true }]
-    },
-    {
-      code: "const {abc, aBd} = someObj;",
-      errors: just("abc", "aBd"),
-      output: "const {aBd, abc} = someObj;",
-      options: [{ caseSensitive: true }]
-    },
-    {
-      code: "const {b, a, ...rest} = someObj;",
-      errors: just("b", "a"),
-      output: "const {a, b, ...rest} = someObj;"
-    },
-    {
-      code: `
-        function foo({
-            b = false,
-            a,
-            ...rest
-        }) {}
-      `,
-      errors: just("b", "a"),
-      output: `
-        function foo({
-            a,
-            b = false,
-            ...rest
-        }) {}
-      `
-    },
-    {
-      code: "const {b, ['a']: x, z} = someObj;",
-      errors: just("b", "a"),
-      output: "const {['a']: x, b, z} = someObj;"
-    },
-    {
-      code: `
         const {
           a,
           b,
-          ['d']: y,
           ['c']: x,
+          ['d']: y,
           [\`\${e}foo\`]: z
         } = someObj;
       `,
-      errors: just("d", "c"),
-      output: `
+      `
         const {
           a,
-          b,
-          ['c']: x,
-          ['d']: y,
-          [\`\${e}foo\`]: z
+          [\`\${e}foo\`]: y,
+          [\`\${d}foo\`]: z
+        } = someObj;
+      `,
+      `
+        const {
+          a,
+          [\`\${d}foo\`]: z,
+          [\`\${e}foo\`]: y
         } = someObj;
       `
-    }
-  ]
+    ],
+    invalid: [
+      {
+        code: "const {[b]: c, a} = someObj;",
+        errors: just("b", "a"),
+        output: "const {a, [b]: c} = someObj;"
+      },
+      {
+        code: `
+          const {
+            a,
+            b,
+            ['d']: y,
+            ['c']: x,
+            [\`\${e}foo\`]: z
+          } = someObj;
+        `,
+        errors: just("d", "c"),
+        output: `
+          const {
+            a,
+            b,
+            ['c']: x,
+            ['d']: y,
+            [\`\${e}foo\`]: z
+          } = someObj;
+        `
+      }
+    ]
+  });
+
+  describe("options", () => {
+    testsFor("caseSensitive", {
+      valid: [
+        {
+          code: "const {a, b} = someObj;",
+          options: [{ caseSensitive: true }]
+        },
+        {
+          code: "const {B, a} = someObj;",
+          options: [{ caseSensitive: true }]
+        },
+        {
+          code: "const {aCc, abb} = someObj;",
+          options: [{ caseSensitive: true }]
+        }
+      ],
+      invalid: [
+        {
+          code: "const {b, a} = someObj;",
+          errors: just("b", "a"),
+          output: "const {a, b} = someObj;",
+          options: [{ caseSensitive: true }]
+        },
+        {
+          code: "const {a, B} = someObj;",
+          errors: just("a", "B"),
+          output: "const {B, a} = someObj;",
+          options: [{ caseSensitive: true }]
+        },
+        {
+          code: "const {abc, aBd} = someObj;",
+          errors: just("abc", "aBd"),
+          output: "const {aBd, abc} = someObj;",
+          options: [{ caseSensitive: true }]
+        }
+      ]
+    });
+  });
 });
